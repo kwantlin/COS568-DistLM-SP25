05a3374ccdabac0360f4bcf20a45cd18ecf94c88
Kathryn L. Wantlin <kw2960@rinse.cs.princeton.edu>
2025-03-27 13:54:25 -0400

    task 1 only log loss for first 5 minibatches

--- task1/run_glue.py
+++ task1/run_glue.py
@@ -125,7 +125,8 @@ def train(args, train_dataset, model, tokenizer):
             loss = outputs[0]  # model outputs are always tuple in pytorch-transformers (see doc)
 
             # Print loss for the first five minibatches
-            logger.info("Minibatch %d, Loss: %f", step, loss.item())
+            if step < 5:
+                logger.info("Minibatch %d, Loss: %f", step, loss.item())
 
             if args.gradient_accumulation_steps > 1:
                 loss = loss / args.gradient_accumulation_steps
